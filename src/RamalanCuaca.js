6ce12280418d4bea5d921fd1f2cd18b22e717294
koropati <windowsdewa@gmail.com>
2018-03-25 01:02:46 +0700

    first commit

--- src/RamalanCuaca.js
+++ src/RamalanCuaca.js
@@ -133,6 +133,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
+    marginBottom: 10,
     //justifyContent: 'space-around',
     //alignItems: 'center',
     flexDirection: 'row'
