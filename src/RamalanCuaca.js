eeb3c9cf8c97dfbed76bf1fb988641a183268e7b
koropati <windowsdewa@gmail.com>
2018-03-26 22:13:53 +0700

    first commit

--- src/RamalanCuaca.js
+++ src/RamalanCuaca.js
@@ -1,49 +1,92 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, Button, TextInput, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, Button, TextInput, View, Image, ActivityIndicator } from 'react-native';
 //7cc62b49fe5d01dd68d03afa3ebceb0c 
+//const iconTemp = require('./icon/thermometer.png');
+const iconTemp = require('./icon/thermometer.png');
+const iconWind = require('./icon/wind.png');
+const iconMain = require('./icon/sun.png');
+const iconDesc = require('./icon/sunny.png');
+const iconSunrise = require('./icon/sunrise.png');
+const iconSunset = require('./icon/sunset.png');
+const iconPressure = require('./icon/pressure.png');
+const iconHumidity = require('./icon/humidity.png');
+const iconSeaLvl = require('./icon/sea.png');
+const iconGndLvl = require('./icon/grass.png');
+
 export default class RamalanCuaca extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       city:'',
+      showLoading: true,
       forecast:{
         main: '-',
         description: '-',
-        temp: 0
+        temp: 0,
+        sunrise: 0,
+        sunset: 0,
+        pressure: 0,
+        humidity: 0,
+        sealvl: 0,
+        gndLvl: 0,
+        speed: 0,
       }
     };
   }
 
-  getWeather= () =>{
-  let url = 'http://api.openweathermap.org/data/2.5/weather?q='+this.state.city+ '&appid=7cc62b49fe5d01dd68d03afa3ebceb0c&units=metric';
-  fetch (url)
-  .then((response) => response.json())
-  .then((responseJson) => {
-    //console.log(responseJson);
-    this.setState({
+  componentWillMount(){
+    setTimeout(()=>{
+      this.setState({
+        showLoading: false
+      })
+    },
+    3000)
+  }
+
+  async getWeather(){
+
+  try{
+ 
+    let response = await fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q='+this.state.city+ '&appid=7cc62b49fe5d01dd68d03afa3ebceb0c&units=metric'
+
+    );
+
+    let responseJson = await response.json();
+    return this.setState({
       forecast: {
-        main: responseJson.weather[0].main,
-        description: responseJson.weather[0].description,
-        temp: responseJson.main.temp
+          main: responseJson.weather[0].main,
+          description: responseJson.weather[0].description,
+          temp: responseJson.main.temp,
+          sunrise: responseJson.sys.sunrise,
+          sunset: responseJson.sys.sunset,
+          pressure: responseJson.main.pressure,
+          humidity: responseJson.main.humidity,
+          seaLvl: responseJson.main.sea_level,
+          gndLvl: responseJson.main.grnd_level,
+          speed: responseJson.wind.speed
       }
     });
-  });
+  }catch (error){
+    console.error(error)
+  }
 }
 
 
   render() {
     return (
+
     <View style={styles.containerMain}>
 
       <View style={styles.boxHeader}>
-          <Text style={{ textAlign: 'center', padding: 30, fontSize: 25, color: '#E8F5E9'}} >Ramalan Cuaca</Text>
+          <Text style={{ textAlign: 'center', padding: 45, fontSize: 25, color: '#E8F5E9'}} >Ramalan Cuaca</Text>
       </View>
 
-      <View style={styles.box5}>
-          <Text style={{ textAlign: 'center', padding: 3, fontSize: 20 , color: '#E8F5E9'}}>Masukan Nama Kota</Text>
+      <View style={styles.boxPencarian}>
+          <Text style={{ textAlign: 'center', padding: 1, fontSize: 20 , color: '#E8F5E9'}}>Masukan Nama Kota</Text>
           
           <View style={styles.textBoxStyle}>
-          <TextInput style = {{height: 50}}
+          <TextInput style = {{height: 40}}
               placeholder="Masukkan Nama Kota"
               onChangeText={(city)=>this.setState({city})}
           />
@@ -63,16 +106,101 @@ export default class RamalanCuaca extends React.Component {
 
       
       <View style={styles.isiTengah}>
-        <Text style = {{padding: 10, fontSize: 20}} >
-          Kota = {this.state.city} {"\n"}
-          Cuaca = {this.state.forecast.main} {"\n"}
-          Description = {this.state.forecast.description} {"\n"}
-          Temp = {this.state.forecast.temp} {"'Celcius"}
-        </Text>
+        
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconTemp} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Temp : {this.state.forecast.temp} 'C</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconWind} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Wind Speed: {this.state.forecast.speed}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconMain} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Main : {this.state.forecast.main}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconDesc} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Main Desc : {this.state.forecast.description}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconSunrise} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Sunrise : {this.state.forecast.sunrise}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconSunset} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Sunset : {this.state.forecast.sunset}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconPressure} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Pressure : {this.state.forecast.pressure}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconHumidity} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Humidity : {this.state.forecast.humidity} % </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconSeaLvl} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Sea Level : {this.state.forecast.seaLvl}</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.boxItem}>
+          <View style={styles.BoxItemInner}>
+            <Image source={iconGndLvl} style={styles.boxIcon} />
+            <View style={styles.boxItemValue}>
+              <Text style = {{padding: 2, fontSize: 16}} >Ground Level : {this.state.forecast.gndLvl}</Text>
+            </View>
+          </View>
+        </View>
+
       </View>
       
       <View style={styles.boxFotter}>
-          <Text style={{ textAlign: 'center', padding: 18, fontSize: 14}} >Copy Right DEWOK@App</Text>
+          <Text style={{ textAlign: 'center', padding: 3, fontSize: 14}} >Copy Right DEWOK@App</Text>
       </View>
 
 </View>
@@ -88,69 +216,33 @@ const styles = StyleSheet.create({
     flexDirection: 'column'
   },
   boxFotter: {
-    height: 60,
+    height: '10%',
     backgroundColor: '#4CAF50',
   },
   boxHeader: {
-    height: 80,
+    height: '15%',
     backgroundColor: '#4CAF50',
   },
-  box2: {
-    flex: 1,
-    backgroundColor: '#A5D6A7',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  box3: {
-    flex: 1,
-    backgroundColor: '#81C784',
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  box4: {
-    flex: 1,
-    backgroundColor: '#81C784',
-    //marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  box5: {
-    flex: 0.7,
+  
+  boxPencarian: {
+    //flex: 0.7,
+    height: '20%',
     backgroundColor: '#4CAF50',
     margin: 10
   },
   isiTengah: {
-    flex: 1,
-    backgroundColor: '#81C784',
-    marginTop: 10,
+    //flex: 1,
+    height: '55%',
+    //backgroundColor: '#81C784',
+    //marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
     marginBottom: 10,
     //justifyContent: 'space-around',
     //alignItems: 'center',
-    flexDirection: 'row'
-  },
-  info: {
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4CAF50'
-  },
-  infoRight: {
-    width: 150,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4CAF50'
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    //padding: 5
   },
   buttonStyle: {
     flex: 1,
@@ -160,11 +252,41 @@ const styles = StyleSheet.create({
   },
   textBoxStyle: {
     //flex: 1,
-    marginTop: 20,
+    marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
     justifyContent: 'center',
     backgroundColor: '#C8E6C9'
+  },
+  boxItem: {
+    width: '50%',
+    height: '20%',
+    padding: 5,
+  },
+  BoxItemInner: {
+    flex: 1,
+    backgroundColor: '#27ae60',
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  boxIcon: {
+    width: '35%',
+    height: '100%',
+    backgroundColor: '#B9F6CA',
+    justifyContent: 'center', 
+  },
+  boxItemValue: {
+    width: '65%',
+    height: '100%',
+    justifyContent: 'center', 
+  },
+  containerLoading:{
+    padding: '50%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   }
+
 
 });
